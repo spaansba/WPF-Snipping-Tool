055f7e550175393a43c7e9665c0c32f3032b268f
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2024-01-17 16:52:28 +0100

    Test

---
+++ Snipping Tool WPF/Modules/IconAndContentData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snipping_Tool_WPF
+{
+    public class IconAndContentData
+    {
+        public object? Icon { get; set; }
+        public object? Content { get; set; }
+    }
+}
